a3d55c95a21a9a007b600db8e7f08d4512c94138
unaxartu <unaxergobi@gmail.com>
2026-05-27 21:54:35 +0200

    chore: created the connection to the postgresql db

--- apps/api/app/services/services.py
+++ apps/api/app/services/services.py
@@ -1,5 +1,5 @@
 import os
-
+import psycopg2
 from minio import Minio
 from minio.error import S3Error
 
@@ -29,7 +29,9 @@ class MinioClient:
 
 class PostgreClient:
     def __init__(self):
-        self.postgre_url = os.getenv("POSTGRE_URL", "localhost")
-        self.postgre_user = os.getenv("POSTGRE_USER", "postgres")
-        self.postgre_pass = os.getenv("POSTGRE_PASS", "")
-        self.postgre_db = os.getenv("POSTGRE_DB", "postgres")
+        postgre_url = os.getenv("POSTGRE_URL", "localhost")
+        postgre_user = os.getenv("POSTGRE_USER", "postgres")
+        postgre_pass = os.getenv("POSTGRE_PASS", "")
+        postgre_db = os.getenv("POSTGRE_DB", "postgres")
+
+        self.postgresql_client = psycopg2.connect(ddbname=postgre_db,user=postgre_user,password=postgre_pass,host=postgre_url,port="5432")
